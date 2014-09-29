6e33cca24787a45a567e04601c86d18f9cfc15c6
Robin Ricard <ricard.robin@gmail.com>
2014-09-29 21:25:15 +0200

    Spice up the Android calls

---
+++ commons/src/main/java/com/anyfetch/companion/android/AndroidSpiceService.java
@@ -0,0 +1,34 @@
+package com.anyfetch.companion.android;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.octo.android.robospice.SpiceService;
+import com.octo.android.robospice.networkstate.NetworkStateChecker;
+import com.octo.android.robospice.persistence.CacheManager;
+import com.octo.android.robospice.persistence.exception.CacheCreationException;
+
+/**
+ * Provides a Service for local async tasks such as Android content providers queries
+ */
+public class AndroidSpiceService extends SpiceService {
+    @Override
+    public CacheManager createCacheManager(Application application) throws CacheCreationException {
+        return new CacheManager();
+    }
+
+    @Override
+    protected NetworkStateChecker getNetworkStateChecker() {
+        return new NetworkStateChecker() {
+            @Override
+            public boolean isNetworkAvailable(Context context) {
+                return true;
+            }
+
+            @Override
+            public void checkPermissions(Context context) {
+
+            }
+        };
+    }
+}
---
+++ commons/src/main/java/com/anyfetch/companion/android/GetEventRequest.java
@@ -0,0 +1,26 @@
+package com.anyfetch.companion.android;
+
+import android.content.Context;
+
+import com.octo.android.robospice.request.SpiceRequest;
+
+/**
+ * Gets an event
+ */
+public class GetEventRequest extends SpiceRequest<Event> {
+
+    private final long mId;
+    private Context mContext;
+
+    public GetEventRequest(Class<Event> klass, Context context, long id) {
+        super(klass);
+        mContext = context;
+        mId = id;
+    }
+
+    @Override
+    public Event loadDataFromNetwork() throws Exception {
+        return Event.getEvent(mContext, mId);
+    }
+}
+
