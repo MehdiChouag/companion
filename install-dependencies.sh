4335e26b40569729c7b3913b6845fc53fbebdb2a
Neamar <neamar@neamar.fr>
2014-10-02 19:20:55 +0200

    Specify ABI

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -12,6 +12,6 @@ if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t build-tools-20.0.0 &&
   echo y | android update sdk -u -a -t sys-img-x86-android-20 &&
   echo y | android update sdk -u -a -t addon-google_apis-google-20 &&
-  echo n | android create avd -n testing -f -t android-20 &&
+  echo n | android create avd -n testing -f -t android-20 --abi armeabi-v7a &&
   touch $DEPS
 fi
