744952f8c704a8e6fbb006c75dafb45e2f3011a9
Robin Ricard <ricard.robin@gmail.com>
2014-10-25 13:47:27 +0200

    ContextFragment => Toolbar

--- mobile/src/main/java/com/anyfetch/companion/fragments/ContextFragment.java
+++ mobile/src/main/java/com/anyfetch/companion/fragments/ContextFragment.java
@@ -1,20 +1,26 @@
 package com.anyfetch.companion.fragments;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.anyfetch.companion.R;
 import com.anyfetch.companion.adapters.DocumentsListAdapter;
+import com.anyfetch.companion.commons.android.pojo.Event;
 import com.anyfetch.companion.commons.api.HttpSpiceService;
 import com.anyfetch.companion.commons.api.builders.ContextualObject;
 import com.anyfetch.companion.commons.api.builders.DocumentsListRequestBuilder;
 import com.anyfetch.companion.commons.api.pojo.DocumentsList;
 import com.anyfetch.companion.commons.api.requests.GetDocumentsListRequest;
+import com.anyfetch.companion.commons.notifications.MeetingPreparationAlarm;
 import com.octo.android.robospice.SpiceManager;
 import com.octo.android.robospice.persistence.DurationInMillis;
 import com.octo.android.robospice.persistence.exception.SpiceException;
@@ -25,7 +31,7 @@ import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 /**
  * Stores the context around an given context (Event, Person, …)
  */
-public class ContextFragment extends Fragment implements RequestListener<DocumentsList>, DialogFragmentChangeListener, SwipeRefreshLayout.OnRefreshListener {
+public class ContextFragment extends Fragment implements RequestListener<DocumentsList>, DialogFragmentChangeListener, SwipeRefreshLayout.OnRefreshListener, Toolbar.OnMenuItemClickListener, View.OnClickListener {
     public static final String ARG_CONTEXTUAL_OBJECT = "contextualObject";
 
     private SpiceManager mSpiceManager = new SpiceManager(HttpSpiceService.class);
@@ -34,6 +40,7 @@ public class ContextFragment extends Fragment implements RequestListener<Documen
     private DocumentsListAdapter mListAdapter;
     private StickyListHeadersListView mListView;
     private SwipeRefreshLayout mSwipeLayout;
+    private Toolbar mToolbar;
 
 
     public ContextFragment() {
@@ -78,6 +85,14 @@ public class ContextFragment extends Fragment implements RequestListener<Documen
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_context, container, false);
+
+        mToolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        mToolbar.setOnMenuItemClickListener(this);
+        mToolbar.setTitle(mContextualObject.getTitle());
+        mToolbar.setNavigationIcon(R.drawable.ic_action_back);
+        mToolbar.setNavigationOnClickListener(this);
+        mToolbar.inflateMenu(R.menu.context);
+
         mListView = (StickyListHeadersListView) view.findViewById(R.id.listView);
 
         mSwipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
@@ -122,5 +137,41 @@ public class ContextFragment extends Fragment implements RequestListener<Documen
     @Override
     public void onRefresh() {
         startQuery(false);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_prepare_on_wear:
+                if (mContextualObject instanceof Event) {
+                    Intent i = new Intent();
+                    i.setAction("com.anyfetch.companion.SHOW_NOTIFICATION");
+                    i.putExtra(MeetingPreparationAlarm.ARG_EVENT, (Event) mContextualObject);
+                    getActivity().sendBroadcast(i);
+                }
+                break;
+            case R.id.action_improve_context:
+                if (mContextualObject instanceof Event) {
+                    Event event = (Event) mContextualObject;
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+                    if (prev != null) {
+                        ft.remove(prev);
+                    }
+                    ft.addToBackStack(null);
+
+                    PersonChooserFragment chooser = PersonChooserFragment.newInstance(event.getAttendees());
+                    chooser.setFragmentChangeListener(this);
+                    chooser.show(ft, "dialog");
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) { // on navigation item
+        getActivity().finish();
     }
 }
