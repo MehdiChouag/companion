4470b41ba6fb31ac6fc223bc945181fa760396fd
Robin Ricard <ricard.robin@gmail.com>
2014-10-27 17:30:15 +0100

    Change cards layout

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -7,11 +7,11 @@ DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
   cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-  echo y | android update sdk -u -a -t android-L > /dev/null &&
+  echo y | android update sdk -u -a -t android-21 > /dev/null &&
   echo y | android update sdk -u -a -t android-19 > /dev/null &&
   echo y | android update sdk -u -a -t android-20 > /dev/null &&
   echo y | android update sdk -u -a -t platform-tools > /dev/null &&
-  echo y | android update sdk -u -a -t build-tools-20.0.0 > /dev/null &&
+  echo y | android update sdk -u -a -t build-tools-21.0.0 > /dev/null &&
   echo y | android update sdk -u -a -t sys-img-x86-android-19 > /dev/null &&
   echo y | android update sdk -u -a -t addon-google_apis-google-18 > /dev/null &&
   echo y | android update sdk -u -a -t extra-google-m2repository > /dev/null &&
