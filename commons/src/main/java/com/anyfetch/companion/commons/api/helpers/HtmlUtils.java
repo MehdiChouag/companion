0e47a3c74f484ec67f71f1c14774b91c0a9a1cf0
Robin Ricard <ricard.robin@gmail.com>
2014-11-06 18:22:50 +0100

    Spaces

--- commons/src/main/java/com/anyfetch/companion/commons/api/helpers/HtmlUtils.java
+++ commons/src/main/java/com/anyfetch/companion/commons/api/helpers/HtmlUtils.java
@@ -3,38 +3,37 @@ package com.anyfetch.companion.commons.api.helpers;
 import android.content.Context;
 import android.util.Log;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
 /**
  * Tools for helping with HTML-Related stuff
  */
 public class HtmlUtils {
-	public static final String DOCUMENT_PLACEHOLDER = "{{document}}";
-	public static String baseDocumentHtml = null;
+    public static final String DOCUMENT_PLACEHOLDER = "{{document}}";
+    public static String baseDocumentHtml = null;
 
-	public static String renderDocument(Context context, String document) {
-		if(baseDocumentHtml == null) {
-			// Preload the HTML file from assets
-			try {
-				InputStream fin = context.getAssets().open("document.html");
-				byte[] buffer = new byte[fin.available()];
-				fin.read(buffer);
-				fin.close();
+    public static String renderDocument(Context context, String document) {
+        if (baseDocumentHtml == null) {
+            // Preload the HTML file from assets
+            try {
+                InputStream fin = context.getAssets().open("document.html");
+                byte[] buffer = new byte[fin.available()];
+                fin.read(buffer);
+                fin.close();
 
-				baseDocumentHtml = new String(buffer);
-			} catch (IOException e) {
-				Log.e("WTF", e.toString());
-				baseDocumentHtml = DOCUMENT_PLACEHOLDER;
-			}
-		}
+                baseDocumentHtml = new String(buffer);
+            } catch (IOException e) {
+                Log.e("WTF", e.toString());
+                baseDocumentHtml = DOCUMENT_PLACEHOLDER;
+            }
+        }
 
-		return baseDocumentHtml.replace(DOCUMENT_PLACEHOLDER, document);
-	}
+        return baseDocumentHtml.replace(DOCUMENT_PLACEHOLDER, document);
+    }
 
     public static String convertHlt(String origin) {
         return origin.replaceAll("<span[^>]+?anyfetch-hlt[^>]+?>(.+?)</span>", "<b>$1</b>");
