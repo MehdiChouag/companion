59951a3a820701512f146a813434305fd674c595
Neamar <neamar@neamar.fr>
2014-11-06 18:14:43 +0100

    Spaces

--- commons/src/main/java/com/anyfetch/companion/commons/api/helpers/HtmlUtils.java
+++ commons/src/main/java/com/anyfetch/companion/commons/api/helpers/HtmlUtils.java
@@ -15,38 +15,38 @@ import javax.xml.parsers.DocumentBuilderFactory;
  * Tools for helping with HTML-Related stuff
  */
 public class HtmlUtils {
-	public static final String DOCUMENT_PLACEHOLDER = "{{document}}";
-	public static final String LOCALE_PLACEHOLDER = "{{locale}}";
-	public static String baseDocumentHtml = null;
+    public static final String DOCUMENT_PLACEHOLDER = "{{document}}";
+    public static final String LOCALE_PLACEHOLDER = "{{locale}}";
+    public static String baseDocumentHtml = null;
 
     /**
      * Will return true if the specified document require some JS to be formatted.
      * @param document
      * @return
      */
-    public static Boolean requireJavascript(String document) {
+    public static Boolean requireaJavascript(String document) {
         return document.contains("anyfetch-date");
     }
 
-	public static String renderDocument(Context context, String document) {
-		if(baseDocumentHtml == null) {
-			// Preload the HTML file from assets
-			try {
-				InputStream fin = context.getAssets().open("document.html");
-				byte[] buffer = new byte[fin.available()];
-				fin.read(buffer);
-				fin.close();
+    public static String renderDocument(Context context, String document) {
+        if(baseDocumentHtml == null) {
+            // Preload the HTML file from assets
+            try {
+                InputStream fin = context.getAssets().open("document.html");
+                byte[] buffer = new byte[fin.available()];
+                fin.read(buffer);
+                fin.close();
 
-				baseDocumentHtml = new String(buffer);
-			} catch (IOException e) {
-				Log.e("WTF", e.toString());
-				baseDocumentHtml = DOCUMENT_PLACEHOLDER;
-			}
-		}
+                baseDocumentHtml = new String(buffer);
+            } catch (IOException e) {
+                Log.e("WTF", e.toString());
+                baseDocumentHtml = DOCUMENT_PLACEHOLDER;
+            }
+        }
 
         String languageCode = Locale.getDefault().getLanguage();
-		return baseDocumentHtml.replace(DOCUMENT_PLACEHOLDER, document).replace(LOCALE_PLACEHOLDER, languageCode);
-	}
+        return baseDocumentHtml.replace(DOCUMENT_PLACEHOLDER, document).replace(LOCALE_PLACEHOLDER, languageCode);
+    }
 
     public static String convertHlt(String origin) {
         return origin.replaceAll("<span[^>]+?anyfetch-hlt[^>]+?>(.+?)</span>", "<b>$1</b>");
