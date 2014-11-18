81cd5e95f211175512d66bd47fafb64838d143c8
Robin Ricard <ricard.robin@gmail.com>
2014-11-18 18:24:19 +0100

    Test builders

---
+++ commons/src/androidTest/java/com/anyfetch/companion/commons/api/builders/DocumentRequestBuilderTest.java
@@ -0,0 +1,68 @@
+package com.anyfetch.companion.commons.api.builders;
+
+import android.content.Context;
+import android.test.InstrumentationTestCase;
+import com.anyfetch.companion.commons.android.pojo.Event;
+import com.anyfetch.companion.commons.android.pojo.Person;
+import com.anyfetch.companion.commons.api.helpers.BaseRequest;
+import com.anyfetch.companion.commons.api.pojo.Document;
+import com.anyfetch.companion.commons.api.requests.DeleteImportantDocumentRequest;
+import com.anyfetch.companion.commons.api.requests.GetDocumentRequest;
+import com.anyfetch.companion.commons.api.requests.PostImportantDocumentRequest;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class DocumentRequestBuilderTest extends InstrumentationTestCase {
+    private Context mContext;
+    private Document mDocument;
+    private Event mEvent;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mContext = getInstrumentation().getContext();
+        mDocument = new Document("a", "b", "c", "d", "e", new Date(), "f", "g", "h", true);
+        mEvent = new Event(1, "a", "b", new Date(0), new Date(0), new ArrayList<Person>(), "c");
+    }
+
+    public void test_build_simple() throws Exception {
+        BaseRequest<Document> req = new DocumentRequestBuilder(mContext).setDocument(mDocument).build();
+        assertTrue(req instanceof GetDocumentRequest);
+    }
+
+    public void test_build_withContext() throws Exception {
+        BaseRequest<Document> req = new DocumentRequestBuilder(mContext)
+                .setDocument(mDocument)
+                .setContextualObject(mEvent)
+                .build();
+        assertTrue(req instanceof GetDocumentRequest);
+    }
+
+    public void test_build_mark() throws Exception {
+        BaseRequest<Document> req = new DocumentRequestBuilder(mContext)
+                .actionMark()
+                .setDocument(mDocument)
+                .setContextualObject(mEvent)
+                .build();
+        assertTrue(req instanceof PostImportantDocumentRequest);
+    }
+
+    public void test_build_unmark() throws Exception {
+        BaseRequest<Document> req = new DocumentRequestBuilder(mContext)
+                .actionUnmark()
+                .setDocument(mDocument)
+                .setContextualObject(mEvent)
+                .build();
+        assertTrue(req instanceof DeleteImportantDocumentRequest);
+    }
+
+    public void test_build_toggle() throws Exception {
+        BaseRequest<Document> req = new DocumentRequestBuilder(mContext)
+                .actionToggle()
+                .setDocument(mDocument)
+                .setContextualObject(mEvent)
+                .build();
+        assertTrue(req instanceof DeleteImportantDocumentRequest);
+    }
+}
---
+++ commons/src/androidTest/java/com/anyfetch/companion/commons/api/builders/DocumentsListRequestBuilderTest.java
@@ -0,0 +1,33 @@
+package com.anyfetch.companion.commons.api.builders;
+
+import android.content.Context;
+import android.test.InstrumentationTestCase;
+import com.anyfetch.companion.commons.android.pojo.Event;
+import com.anyfetch.companion.commons.android.pojo.Person;
+import com.anyfetch.companion.commons.api.helpers.BaseRequest;
+import com.anyfetch.companion.commons.api.pojo.DocumentsList;
+import com.anyfetch.companion.commons.api.requests.GetDocumentsListRequest;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class DocumentsListRequestBuilderTest extends InstrumentationTestCase {
+    private Context mContext;
+    private Event mEvent;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mContext = getInstrumentation().getContext();
+        mEvent = new Event(1, "a", "b", new Date(0), new Date(0), new ArrayList<Person>(), "c");
+    }
+
+    public void test_build_simple() throws Exception {
+        BaseRequest<DocumentsList> req = new DocumentsListRequestBuilder(mContext)
+                .setContextualObject(mEvent)
+                .build();
+        assertTrue(req instanceof GetDocumentsListRequest);
+    }
+
+    // TODO: other tests with the important & not important when implemented
+}
