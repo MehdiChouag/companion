7ab0b4237579dc25969aa71a5d45881ab28fb758
Robin Ricard <ricard.robin@gmail.com>
2014-11-18 18:07:25 +0100

    New tests around documents

--- commons/src/androidTest/java/com/anyfetch/companion/commons/api/pojo/TestDocument.java
+++ commons/src/androidTest/java/com/anyfetch/companion/commons/api/pojo/TestDocument.java
@@ -1,12 +1,20 @@
 package com.anyfetch.companion.commons.api.pojo;
 
+import android.os.Bundle;
 import android.test.InstrumentationTestCase;
-
 import com.google.gson.Gson;
 
 import java.util.Date;
 
 public class TestDocument extends InstrumentationTestCase {
+    private Document mDocument;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mDocument = new Document("a", "b", "c", "e", "f", new Date(), "g", "h", "i", true);
+    }
+
     public void test_json() throws Exception {
         Gson gson = new Gson();
         Document doc = gson.fromJson(gson.toJson(new Document("file", "Dropbox", "x", "y", "z", new Date(0), "Doc", "Docu", "Document", false)), Document.class);
@@ -18,5 +26,22 @@ public class TestDocument extends InstrumentationTestCase {
         assertEquals("Doc", doc.getTitle());
         assertEquals("Docu", doc.getSnippet());
         assertEquals("Document", doc.getFull());
+    }
+
+    public void testParcels() throws Exception {
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("parcel", mDocument);
+        Document target = bundle.getParcelable("parcel");
+        assertEquals(mDocument.getTitle(), target.getTitle());
+        assertEquals(mDocument.getDate(), target.getDate());
+        assertEquals(mDocument.isImportant(), target.isImportant());
+    }
+
+    public void test_requireJavascript() throws Exception {
+        Document jsDoc = new Document("a", "b", "c", "e", "f", new Date(), "g", "anyfetch-date", "anyfetch-date", true);
+        assertTrue(jsDoc.fullRequireJavascript());
+        assertTrue(jsDoc.snippetRequireJavascript());
+        assertFalse(mDocument.fullRequireJavascript());
+        assertFalse(mDocument.snippetRequireJavascript());
     }
 }
