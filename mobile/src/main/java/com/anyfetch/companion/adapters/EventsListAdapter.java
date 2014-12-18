f60ad14ebc9a90ff31377b4a27977d149bd39905
Neamar <neamar@neamar.fr>
2014-12-18 18:41:04 +0100

    Layout improvements (i guess)

--- mobile/src/main/java/com/anyfetch/companion/adapters/EventsListAdapter.java
+++ mobile/src/main/java/com/anyfetch/companion/adapters/EventsListAdapter.java
@@ -6,7 +6,6 @@ import android.graphics.drawable.GradientDrawable;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -70,8 +69,8 @@ public class EventsListAdapter extends TimedListAdapter implements StickyListHea
 
         if (event.getColor() != 0) {
             // Set the event color -- we need  to put it to aRGB.
-            FrameLayout mainContainer = (FrameLayout) convertView.findViewById(R.id.main_container);
-            GradientDrawable shapeDrawable = (GradientDrawable) mainContainer.getBackground();
+            View backgroundView = (View) convertView.findViewById(R.id.backgroundView);
+            GradientDrawable shapeDrawable = (GradientDrawable) backgroundView.getBackground();
             int color = 0xff000000 + event.getColor();
             shapeDrawable.setColor(color);
         }
