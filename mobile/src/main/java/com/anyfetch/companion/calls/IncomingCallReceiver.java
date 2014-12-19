c1d1b6b5ff4ac147e5b481549f1139c9cd12302c
Neamar <neamar@neamar.fr>
2014-12-19 17:09:27 +0100

    Discard notification on hangup

--- mobile/src/main/java/com/anyfetch/companion/calls/IncomingCallReceiver.java
+++ mobile/src/main/java/com/anyfetch/companion/calls/IncomingCallReceiver.java
@@ -3,7 +3,6 @@ package com.anyfetch.companion.calls;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v4.app.NotificationManagerCompat;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
@@ -12,12 +11,7 @@ import android.util.Log;
 import com.anyfetch.companion.commons.android.pojo.Person;
 import com.anyfetch.companion.notifications.BuildNotificationStackTask;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class IncomingCallReceiver extends BroadcastReceiver {
-    public static String CALL_JOURNAL = "call_journal";
-
     private static PhoneStateListener stateListener;
 
     public void onReceive(final Context context, Intent intent) {
@@ -52,13 +46,6 @@ public class IncomingCallReceiver extends BroadcastReceiver {
 
                             if (contact != null) {
                                 // Clean up previous notification
-                                SharedPreferences callSettings = context.getSharedPreferences("calls", Context.MODE_PRIVATE);
-                                Set<String> currentCalls = callSettings.getStringSet(IncomingCallReceiver.CALL_JOURNAL, new HashSet<String>());
-                                SharedPreferences.Editor editor = callSettings.edit();
-                                currentCalls.remove(Long.toString(contact.getId()));
-                                editor.putStringSet(IncomingCallReceiver.CALL_JOURNAL, currentCalls);
-                                editor.commit();
-
                                 NotificationManagerCompat.from(context).cancel(contact.getHashCode());
                             }
                         }
--- mobile/src/main/java/com/anyfetch/companion/calls/OutgoingCallReceiver.java
+++ mobile/src/main/java/com/anyfetch/companion/calls/OutgoingCallReceiver.java
@@ -3,14 +3,10 @@ package com.anyfetch.companion.calls;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.anyfetch.companion.commons.android.pojo.Person;
 import com.anyfetch.companion.notifications.BuildNotificationStackTask;
-
-import java.util.HashSet;
-import java.util.Set;
 
 public class OutgoingCallReceiver extends BroadcastReceiver {
     @Override
@@ -26,13 +22,6 @@ public class OutgoingCallReceiver extends BroadcastReceiver {
         else {
             Log.i("Outgoing", "Outgoing call caught: " + contact.getName());
             new BuildNotificationStackTask(context).execute(contact, null, null);
-
-            SharedPreferences callSettings = context.getSharedPreferences("calls", Context.MODE_PRIVATE);
-            Set<String> currentCalls = callSettings.getStringSet(IncomingCallReceiver.CALL_JOURNAL, new HashSet<String>());
-            SharedPreferences.Editor editor = callSettings.edit();
-            currentCalls.add(Long.toString(contact.getId()));
-            editor.putStringSet(IncomingCallReceiver.CALL_JOURNAL, currentCalls);
-            editor.commit();
         }
     }
 }
