50bc70c1dbfcc0357aa368246f226c21200ae149
buathi_q <buathi_q@epitech.eu>
2014-12-30 18:32:31 +0100

    Update script

--- convert.sh
+++ convert.sh
@@ -5,8 +5,8 @@ git clone git@github.com:AnyFetch/anyfetch-assets.git /tmp/anyfetch-assets;
 
 for id in `ls /tmp/anyfetch-assets/images/providers | grep -v grayscale | sed "s/\.svg//g"`;
 do
-    convert -resize 48x48 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-mdpi/ic_${id}.png;
-    convert -resize 72x72 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-hdpi/ic_${id}.png;
-    convert -resize 96x96 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-xhdpi/ic_${id}.png;
-    convert -resize 144x144 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-xxhdpi/ic_${id}.png;
+    convert -resize 48x48 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-mdpi/ic_provider_${id}.png;
+    convert -resize 72x72 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-hdpi/ic_provider_${id}.png;
+    convert -resize 96x96 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-xhdpi/ic_provider_${id}.png;
+    convert -resize 144x144 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-xxhdpi/ic_provider_${id}.png;
 done;
