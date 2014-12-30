fcf4fcb0d180551d50b7157aabc0eed735a90a51
buathi_q <buathi_q@epitech.eu>
2014-12-30 18:45:50 +0100

    Change quality

--- convert.sh
+++ convert.sh
@@ -5,8 +5,8 @@ git clone git@github.com:AnyFetch/anyfetch-assets.git /tmp/anyfetch-assets;
 
 for id in `ls /tmp/anyfetch-assets/images/providers | grep -v grayscale | sed "s/\.svg//g"`;
 do
-    convert -background none -resize 48x48 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-mdpi/ic_provider_${id}.png;
-    convert -background none -resize 72x72 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-hdpi/ic_provider_${id}.png;
-    convert -background none -resize 96x96 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-xhdpi/ic_provider_${id}.png;
-    convert -background none -resize 144x144 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-xxhdpi/ic_provider_${id}.png;
+    convert -background none -quality 80 -resize 48x48 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-mdpi/ic_provider_${id}.png;
+    convert -background none -quality 80 -resize 72x72 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-hdpi/ic_provider_${id}.png;
+    convert -background none -quality 80 -resize 96x96 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-xhdpi/ic_provider_${id}.png;
+    convert -background none -quality 80 -resize 144x144 /tmp/anyfetch-assets/images/providers/${id}.svg ./commons/src/main/res/drawable-xxhdpi/ic_provider_${id}.png;
 done;
